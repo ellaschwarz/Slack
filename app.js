b4628b93bc620dfba6ced5077f53813ceb6acbe9
Ella Schwarz <ellaschwarzt@gmail.com>
2019-12-07 13:20:51 +0100

    Added some dependencies, added a temporary input-field for username, added server.js file with basic code to create a database

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var ejs = require('ejs');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const ejs = require('ejs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const requestPromise = require('request-promise');
 
-app.set('views', './views');
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
---
+++ server.js
@@ -0,0 +1,33 @@
+const httpError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const server = express();
+
+
+let mongo = require("mongodb");
+let monk = require("monk");
+let bodyParser = require("body-parser");
+var usersDB = monk('localhost:27017/users');
+
+
+server.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
+server.use(bodyParser.json());
+
+
+server.use(function (req, res, next) {
+  req.db = usersDB;
+  next();
+});
+
+
+server.use(logger('dev'));
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(cookieParser());
+server.use(express.static(path.join(__dirname, 'public')));
