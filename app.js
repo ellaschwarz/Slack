88539a28ba8349bd4320e9396f1b53f29efd1065
Johan Garzon <johanprograma@hotmail.com>
2019-12-11 00:20:35 +0100

    Login works wheter Username or Email // TODO: take away console.logs after asking someone WHY it does not work with IF statement!

--- app.js
+++ app.js
@@ -21,22 +21,19 @@ const initializePassport = require('./passport-config');
 initializePassport(
     passport,
     // email => users.find(user => user.email === email),
-    // cambio
-    mailOrUser => {
-        if (mailOrUser.search('@') < 0) {
-            console.log('It is NOT a mail: ' + mailOrUser);
-            return users.find(user => { user.name === mailOrUser;
-            console.log('user: ' + user)
-            console.log('user.name: ' + user.name)
-            console.log(user.name === mailOrUser) })
-        } else {
-            console.log('It is a mail: ' + mailOrUser);
-            return users.find(user => { user.email === mailOrUser;
-            console.log('user: ' + user)
-            console.log('user.email: ' + user.email) 
-            console.log(user.email === mailOrUser) })
-        }
-    },
+
+    mailOrUser => 
+        mailOrUser.search('@') < 0 ?
+        users.find(user => user.name === mailOrUser) :
+        users.find(user => user.email === mailOrUser),
+
+/*     mailOrUser => {
+        if (mailOrUser.search('@') < 0) 
+            users.find(user => user.name === mailOrUser);
+        else
+            users.find(user => user.email === mailOrUser);
+    }, */
+
     id => users.find(user => user.id === id)
   )
 
